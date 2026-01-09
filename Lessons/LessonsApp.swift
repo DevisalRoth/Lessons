849083dff31858c441dc04e8682cac78a173349e
Visalroth <chouvisalroth9999@gmail.com>
2026-01-09 09:16:40 +0700

    Initial Commit

---
+++ Lessons/LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonsApp.swift
+//  Lessons
+//
+//  Created by Visalroth on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
